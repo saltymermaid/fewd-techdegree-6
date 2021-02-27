a31607d091b27d0caff10a5f1de757fd9683bf28
Melissa Blades <melissa@saltymermaid.dev>
2021-02-27 09:46:11 -0500

    Check for win

--- js/guessWhat.js
+++ js/guessWhat.js
@@ -1,12 +1,11 @@
 const keyboard = document.getElementById('qwerty');
-// const pressedKey = keyboard.value;
 const phrase = document.querySelector('#phrase > ul');
 const resetButton = document.getElementById('start-game');
 const overlay = document.getElementById('overlay');
 const scoreboard = document.getElementById('scoreboard');
 let missed = 0;
 
-resetButton.addEventListener('click', (event) => {
+resetButton.addEventListener('click', () => {
   overlay.style.display = 'none';
 });
 
@@ -34,7 +33,7 @@ addPhraseToDisplay(phraseArray);
 
 function checkLetter(pressedKey) {
   let selectedLetter = null
-  allLetters = document.querySelectorAll('#phrase > ul > li.letter')
+  const allLetters = document.querySelectorAll('#phrase > ul > li.letter')
   allLetters.forEach(letter => {
     if(letter.innerHTML.toLowerCase() === pressedKey) {
       letter.classList.add("show")
@@ -44,6 +43,16 @@ function checkLetter(pressedKey) {
   return selectedLetter
 }
 
+function updateOverlay(status, message) {
+  overlay.style.display = 'flex';
+  overlay.setAttribute("class", status);
+  const p = document.createElement("p");
+  overlay.appendChild(p);
+  text = message;
+  p.textContent = text;
+  resetButton.textContent = "Play again";
+}
+
 keyboard.addEventListener('click', (e) => {
   let pressedKey = (e.target.innerHTML).toLowerCase();
   e.target.setAttribute("class", "chosen");
@@ -51,6 +60,21 @@ keyboard.addEventListener('click', (e) => {
   let letterFound = checkLetter(pressedKey);
   if(!letterFound) {
     missed += 1;
-    console.log(missed); // update hearts
+    let hearts = document.querySelectorAll('li.tries > img')
+    for (let i = 0; i < missed; i++) {
+      hearts[i].setAttribute("src", "images/lostHeart.png")
+    }
+  }
+  let status = checkWin()
+  if (status) {
+    updateOverlay("win", 'You won!');
+  } else if (missed >= 5) {
+    updateOverlay("lose", 'You lost!');
   }
 })
+
+function checkWin() {
+  const allLetters = document.querySelectorAll('#phrase > ul > li.letter')
+  const displayedLetters = document.querySelectorAll('#phrase > ul > li.show')
+  return allLetters.length === displayedLetters.length
+}
